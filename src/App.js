9666bfa55db4261ae7fd4971f5771583f148f05d
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-11 12:48:24 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  PDFViewer,
+  Page,
+  Text,
+  Image,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
 
-function App() {
+const styles = StyleSheet.create({
+  pageContainer: {
+    border: "1px solid black",
+    width: "100%",
+    height: "600px",
+    boxSizing: "border-box",
+    padding: "10px",
+  },
+  page: {
+    padding: 5,
+    flexDirection: "column",
+    alignItems: "center",
+    border: "1px solid black",
+    backgroundColor: "#f2f2f2",
+    boxSizing: "border-box",
+  },
+  image: {
+    width: 10,
+  },
+  title: {
+    fontSize: 2,
+  },
+});
+
+function MyDocument() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Document>
+      <Page size="A8" style={styles.page}>
+        <Text style={styles.title}>Hello, World!</Text>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png"
+          style={styles.image}
+        />
+      </Page>
+    </Document>
   );
 }
 
-export default App;
+function PDFGenerator() {
+  return (
+    <PDFViewer
+      width="100%"
+      height="600"
+      scale="100%"
+      style={{
+        border: "none",
+        boxShadow: "none",
+        overflow: "auto",
+      }}
+    >
+      <MyDocument />
+    </PDFViewer>
+  );
+}
+
+export default PDFGenerator;
